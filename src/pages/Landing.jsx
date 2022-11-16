249c7953518bd75d24ebbd74ec3c62bb00c205c8
Charles Ong <charlesong173@gmail.com>
2022-11-16 12:42:42 +0800

    first commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// Components
+
+// CSS
+
+// Page Function
+function Landing() {
+
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+  
+export default Landing
+  
