9f4302934e87bed0f5e9e6b7b69fe131a5b63211
Charles Ong <charlesong173@gmail.com>
2022-11-16 14:55:42 +0800

    added mapbox api

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxlc29uZyIsImEiOiJjbGFqNnh2bDAwOXZlM3ZycWVkZ3YycnlzIn0.o43APqITPr1TxZFDwtClPA';
 
 // Components
 
 // CSS
+import "./styles/Landing.css"
+import "mapbox-gl/dist/mapbox-gl.css";
+// import 'https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css'
 
 // Page Function
 function Landing() {
 
+  const mapContainer = useRef(null)
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    // Change Lat, Lng on move
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    // 
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
+    
+    // Clean up on unmount
+    return () => map.remove();
+  }, []);
+
   return (
     <div>
-      <h1>hello</h1>
+      Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      <div className="map-container" ref={mapContainer}/>
     </div>
   )
 }
