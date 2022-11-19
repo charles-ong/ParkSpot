fe91609065f0aed1ae01119850d8c67b7709dbc2
Charles Ong <charlesong173@gmail.com>
2022-11-19 20:10:00 +0800

    new form for netlify (untested)

--- src/components/UserMarkerSubmission.jsx
+++ src/components/UserMarkerSubmission.jsx
@@ -7,8 +7,8 @@ import Image from 'react-bootstrap/Image'
 function UserMarkerSubmission(props){
 
     const [showModal, setShowModal] = useState(false);
-    const handleClose = () => setShowModal(false);
     const handleShow = () => setShowModal(true)
+    const handleClose = () => setShowModal(false);
     const source = "https://api.mapbox.com/styles/v1/charlesong/clajdaze2000e14qpshcv9szq/static/pin-m-circle+ff0000(" + props.lngLat + ")/auto/600x300@2x?attribution=true&logo=false&access_token=pk.eyJ1IjoiY2hhcmxlc29uZyIsImEiOiJjbGFqNnh2bDAwOXZlM3ZycWVkZ3YycnlzIn0.o43APqITPr1TxZFDwtClPA";
 
     function cancel() {
@@ -32,29 +32,29 @@ function UserMarkerSubmission(props){
                 Submit
             </Button>
     
-            <Modal show={showModal} onHide={handleClose} centered size="sm">
+            <Modal show={showModal} onHide={handleClose} centered size="md">
                 <Modal.Header closeButton>
                 <Modal.Title>Suggest Free Parking</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Image fluid src={source}/>
-                    <Form 
+                    <form 
                         data-netlify="true"
                         name="ParkSpot Marker Submission"
                         method="post"
                     >
-                        <Form.Group className="mb-3">
-                            <Form.Label>Coordinates</Form.Label>
-                            <Form.Control placeholder="Disabled input" disabled name="ParkSpot Marker Submission" value={props.lngLat}/>
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="ParkSpot Marker Submission"/>
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                        </Form.Group>
-                    </Form>
+                        <div className="mb-3 mt-3">
+                            <label>Coordinates </label>
+                            <input placeholder="Disabled input" disabled name="Coordinates" value={props.lngLat}/>
+                        </div>
+                        <div className="mb-3">
+                            <label >Email address </label>
+                            <input type="email" placeholder="Enter email" name="Email" required={true}/>
+                            <p className="text-muted text-xs">
+                            We'll never share your email with anyone else.
+                            </p>
+                        </div>
+                    </form>
                 </Modal.Body>
                 <Modal.Footer>
                 <Button variant="danger" onClick={handleClose}>
