511e9bd0a495c1843c196b06b96b374f1e63108d
Charles Ong <charlesong173@gmail.com>
2022-12-27 18:01:00 +0800

    fix assets

--- registerSW.js
+++ registerSW.js
@@ -1,5 +1,14 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js', { scope: '/' })
-    })
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+          console.log('Service Worker registration successful', registration.scope);
+        }, function(err) {
+          console.log('Service Worker registration failed', err);
+        }).catch(function(err) {
+          console.log(err);
+        });
+      });
+} 
+else {
+  console.log('Service Worker is not supported by browser.');
 }
