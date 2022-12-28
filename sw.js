cd889b853038534555eefcf9f1690732fd372ddf
Charles Ong <charlesong173@gmail.com>
2022-12-28 20:21:46 +0800

    implemented cache clearing in sw

--- sw.js
+++ sw.js
@@ -1,42 +1,15 @@
-let CACHE_NAME = "Arc-teryx";
-let urlsToCache = ["/"];
-
-// Install a service worker
-self.addEventListener("install", (event) => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Cache and return requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-// Update a service worker
-self.addEventListener("activate", (event) => {
-  let cacheWhitelist = ["Arc-teryx"];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+          }).map(function(cacheName) {
             return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+          })
+        );
+      })
+    );
+  });
